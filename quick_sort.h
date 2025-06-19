4df7bb12cbd1bbfac5c98f66d110fe1e996a6ef0
Camerupt65 <evertonmarlonberlezi@gmail.com>
2025-06-19 15:40:32 -0300

    Delete quick_sort.h

--- quick_sort.h
+++
@@ -1,8 +0,0 @@
-#ifndef ORDENACAO_H
-#define ORDENACAO_H
-
-// ordenacao quicksort
-void quickSort(Item **inicio, int crescente);
-void particionar(Item *inicio, Item **menores, Item **maiores, int pivo, int crescente);
-
-#endif
